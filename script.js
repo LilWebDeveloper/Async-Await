69bfbe0122b4fb90e25cc3011f0f782753e3a8c4
Kacper Falecki <kfalecki17@gmail.com>
2022-11-14 18:27:06 +0100

    Finish Version

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('.blogss');
+const container = document.querySelector('.result');
 const renderTable = async () => {
     let users = 'http://localhost:3000/users';
     let companies = 'http://localhost:3000/companies';
@@ -10,26 +10,42 @@ const renderTable = async () => {
     const res2 = await fetch(companies);
     const comp = await res2.json();
 
-    const together = [...user, ...comp];
+    const together = [...user, ...comp]
+       
+
+    const dar = Object.keys(comp)
+
 
     
 
     let template = '';
+
+    user.forEach(el1 => {
+        const elo = Object.values(el1)
+        const elo1 = elo[3]
+        const elo2 = elo1['company']
+
+        comp.forEach(el2 =>{
+            console.log(el2)
+        })
+
+        console.log(elo2)
+    }),
 
 
     user.forEach(post => {
 
             template += `
             <tr>
-               <td>${post.uris.company}</td>
                <td>${post.name}</td>
+               <td>${post.uris.company}</td>
            </tr>
             `
-    })
+    }),
 
     container.innerHTML = template;
 
-    console.log(finde)
+    console.log()
 };
 
 window.addEventListener("DOMContentLoaded", () => renderTable());
