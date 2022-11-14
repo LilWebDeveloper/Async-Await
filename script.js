b731429c503da81644b1589ac58976d679052f45
Kacper Falecki <kfalecki17@gmail.com>
2022-11-14 18:27:40 +0100

    Finish Vers

--- script.js
+++ script.js
@@ -2,51 +2,23 @@ const container = document.querySelector('.result');
 const renderTable = async () => {
     let users = 'http://localhost:3000/users';
     let companies = 'http://localhost:3000/companies';
-
-
     const res = await fetch(users);
     const user = await res.json();
-
     const res2 = await fetch(companies);
     const comp = await res2.json();
-
-    const together = [...user, ...comp]
-       
-
-    const dar = Object.keys(comp)
-
-
-    
-
     let template = '';
-
-    user.forEach(el1 => {
-        const elo = Object.values(el1)
-        const elo1 = elo[3]
-        const elo2 = elo1['company']
-
-        comp.forEach(el2 =>{
-            console.log(el2)
-        })
-
-        console.log(elo2)
-    }),
-
-
-    user.forEach(post => {
-
-            template += `
-            <tr>
-               <td>${post.name}</td>
-               <td>${post.uris.company}</td>
-           </tr>
-            `
-    }),
-
-    container.innerHTML = template;
-
-    console.log()
+        for(var i of comp){
+            template += '<tr><td>';
+            template += i.name;
+            template += '</td><td>';
+            for(var j of user){
+                if(j.uris.company === i.uri){
+                    template += j.name + '<br>';
+                }
+            }
+            template += "</td></tr>";
+        }
+        container.innerHTML = template;
 };
-
 window.addEventListener("DOMContentLoaded", () => renderTable());
 
